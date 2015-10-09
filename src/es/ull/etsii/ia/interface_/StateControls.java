bbbc831118b5597ec987e0150e306a7ccd35ef6e
MARTIN HERNANDEZ <alu4550@cc175.etsii.ull.es>
2015-10-09 13:44:16 +0100

    modificado state controls

--- src/es/ull/etsii/ia/interface_/StateControls.java
+++ src/es/ull/etsii/ia/interface_/StateControls.java
@@ -1,28 +1,42 @@
 package es.ull.etsii.ia.interface_;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class StateControls extends JPanel {
+public class StateControls extends JPanel implements ActionListener {
 	private JButton obstacle = new JButton("obst");
 	private JButton goal = new JButton("goal");
 	private JButton initPoint = new JButton("init");
-	private JButton [] buttons = { obstacle, goal, initPoint};
+	private JButton[] buttons = { obstacle, goal, initPoint };
 	private short selected = 0;
-	public StateControls(ActionListener listener){
-		addListener(listener);
+
+	public StateControls(ActionListener listener) {
+		addListener(this);
 		addButtons();
 	}
+
 	protected void addButtons() {
 		add(obstacle);
 		add(goal);
 		add(initPoint);
 	}
-	protected void addListener(ActionListener listener){
+
+	protected void addListener(ActionListener listener) {
 		obstacle.addActionListener(listener);
 		goal.addActionListener(listener);
 		initPoint.addActionListener(listener);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().getClass() == JButton.class) {
+			for (JButton button : buttons) {
+				button.setEnabled(button != e.getSource());
+			}
+		}
+
 	}
 }
